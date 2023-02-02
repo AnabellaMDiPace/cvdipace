4f02702c387e5053d2f71baefdd32bd796703983
Anabella Di Pace <anabelladipace@gmail.com>
2023-02-01 21:06:15 -0300

    mi primer escritura en el repositorio

---
+++ interaccion.js
@@ -0,0 +1,14 @@
+document.getElementById('boton4').onclick = function () {
+    console.log("se ha cliqueado el botón idioma");
+    document.getElementById("idiomas").innerHTML="Idiomas: Ingles básico";
+}
+
+document.getElementById('boton5').onclick = function () {
+    console.log("se ha cliqueado el botón programas");
+    document.getElementById("programas").innerHTML="Programas: Buen manejo de Excel, Word y Power Point";
+}
+
+document.getElementById('boton6').onclick = function () {
+    console.log("se ha cliqueado el botón expectativa laboral");
+    document.getElementById("expectativalaboral").innerHTML="Expectativa laboral: Poder trabajar en un buen ambiente laboral, progresar en mi puesto de trabajo de la mejor manera posible, adentrando y aprendiendo nuevos conocimientos.";
+}
